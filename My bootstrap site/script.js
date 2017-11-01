66953f902780a24db361d1d0f9565699273596be
Michał Szyszka <mszyszka223@gmail.com>
2017-11-01 08:25:28 +0100

    I change some css and fix scrollTop

--- My bootstrap site/script.js
+++ My bootstrap site/script.js
@@ -1,15 +1,16 @@
 //                              TOP BUTTON
 // When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
     scrollFunction()
 };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementsById("myBtn").style.display = "block";
+        document.getElementById("myBtn").style.display = "block";
     }
     else {
-        document.getElementsById("myBtn").style.display = "none";
+        document.getElementById("myBtn").style.display = "none";
     }
 }
 // When the user clicks on the button, scroll to the top of the document
@@ -17,5 +18,4 @@ function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
-//                            END OF TOP BUTTON 
-
+//                            END OF TOP BUTTON
