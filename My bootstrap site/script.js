9b6cacc0fe8e7b7965667cde1afe4fb0e4a3cb24
Michał Szyszka <mszyszka223@gmail.com>
2017-11-01 08:16:58 +0100

    I added some jquery

--- My bootstrap site/script.js
+++ My bootstrap site/script.js
@@ -1,21 +1,21 @@
-
 //                              TOP BUTTON
-
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementsById("myBtn").style.display = "block";
+    }
+    else {
+        document.getElementsById("myBtn").style.display = "none";
     }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
-
 //                            END OF TOP BUTTON 
+
